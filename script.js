7534f503f85172be620b5732b8dcb038454dcd40
SlavTheKoder <valkrukov@gmail.com>
2023-06-09 00:34:39 +0300

    lightswitcher added

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-document.body.innerHTML = '<h1>Hello from JavaScript</h1>';
+document.body.innerHTML = '<h1>One More Disaster Artist</h1>';
 console.log('test2');
+document.body.onclick = function () {
+  document.body.classList.toggle('dark');
+};
